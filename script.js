c64089bc9466b1e9053491fff2e083f5db3b94fd
‪Yousef Shabib‬‏ <s12217432@stu.najah.edu>
2025-08-05 10:48:02 +0300

    Display 50 name,pics

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+
+async function getPokemons() {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0");
+    const data = await response.json();
+    return data.results;
+}
+
+async function getPokemonDetails(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return {
+        name: data.name,
+        image: data.sprites.front_default
+    };
+
+}
+
+async function displayPokemons() {
+    const pokemons = await getPokemons();
+    const grid = document.getElementById("pokemonGrid");
+
+    for (let poke of pokemons) {
+        const details = await getPokemonDetails(poke.url);
+        const card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `
+         <img src="${details.image}" />
+        <h3>${details.name}</h3>
+      `;
+
+        grid.appendChild(card);
+    }
+}
+
+displayPokemons();
