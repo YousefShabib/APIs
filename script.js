a7bac1651fece88379ce5571747b9edd3793ef69
‪Yousef Shabib‬‏ <s12217432@stu.najah.edu>
2025-08-05 16:24:17 +0300

    refactor(not waiting api cards ): add- then- to code

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
-function getPokemons() {
-    return fetch("https://pokeapi.co/api/v2/pokemon?limit=5000&offset=0")
-        .then(response => response.json())
-        .then(data => data.results);
+async function getPokemons() {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0");
+    const data = await response.json();
+    return data.results;
 }
 
-function getPokemonDetails(url) {
-    return fetch(url)
-        .then(response => response.json())
-        .then(data => ({
-            name: data.name,
-            image: data.sprites.front_default
-        }));
+async function getPokemonDetails(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return {
+        name: data.name,
+        image: data.sprites.front_default
+    };
 }
 
-function displayPokemons() {
+async function displayPokemons() {
+    const pokemons = await getPokemons();
     const grid = document.getElementById("pokemonGrid");
 
-    getPokemons().then(pokemons => {
-        pokemons.forEach(poke => {
-            getPokemonDetails(poke.url).then(details => {
-                const card = document.createElement("div");
-                card.className = "card";
+    for (let poke of pokemons) {
+        const details = await getPokemonDetails(poke.url);
+        const card = document.createElement("div");
+        card.className = "card";
 
-                card.innerHTML = `
-                 <img src="${details.image}" />
-                 <h2>${details.name}</h2>
-               `;
+        card.innerHTML = `
+         <img src="${details.image}" />
+         <h3>${details.name}</h3>
+       `;
 
-                grid.appendChild(card);
-            });
-        });
-    });
+        grid.appendChild(card);
+    }
 }
 
 displayPokemons();
 
 document.getElementById("search").addEventListener("input", function (x) {
-    const searchValue = x.target.value.toLowerCase(); 
+    const searchValue = x.target.value.toLowerCase();
     const cards = document.querySelectorAll(".card");
 
     cards.forEach(card => {
-        const name = card.querySelector("h2").innerText.toLowerCase();
+        const name = card.querySelector("h3").innerText.toLowerCase();
         if (name.includes(searchValue)) {
             card.style.display = "block";
         } else {
